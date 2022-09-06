167be15a19ea7651c30fc3b2cafaebdb0b4db5d4
Alp Erbil <beratalp@gmail.com>
2022-09-06 21:25:28 +0300

    fixed a small bug

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -34,6 +34,8 @@ struct CardView: View {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
                 Text(card.content).font(.largeTitle)
+            } else if card.isMatched {
+                shape.hidden()
             } else {
                 shape.fill()
             }
