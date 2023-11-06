fec7fbd8cc4dc04e6ba7e297059ff7bbf75b4fb2
Alp Erbil <beratalp@gmail.com>
2023-11-06 23:12:25 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alp Erbil on 6.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
