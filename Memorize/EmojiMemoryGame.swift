4cd8f9e0da8769ba10d850a5ad504652b80c2347
Alp Erbil <beratalp@gmail.com>
2023-11-19 21:13:40 +0300

    finished game's logic

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Alp Erbil on 18.11.2023.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 11) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+        objectWillChange.send()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,66 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Alp Erbil on 18.11.2023.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        // Add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)b"))
+        }
+    }
+    
+    var indexOfTheOneAnyOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { cards[$0].isFaceUp }.only }
+        set { return cards.indices.forEach {cards[$0].isFaceUp = (newValue == $0)} }
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAnyOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAnyOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+        print(cards)
+    }
+    
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        
+        var isFaceUp = false
+        var isMatched = false
+        let content: CardContent
+        var id: String
+        
+        var debugDescription: String {
+            "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "unmatched")"
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
+    }
+}
